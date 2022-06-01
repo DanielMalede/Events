78759ea5d2e0f47d1b6ddfddec9f94bcdbce4a36
DanielMalede <danielmalede@gmail.com>
2022-06-01 15:27:05 +0300

    events

---
+++ main.js
@@ -0,0 +1,35 @@
+// function popalert() {
+//     alert("whaassaapp")
+// }
+
+// function printMyName() {
+//     console.log("Daniel Malede");
+// }
+
+// function changeSpanColor() {
+//     var changecolor = document.getElementById("span_Color")
+//     changecolor.style = "color: blue"
+// }
+
+// function changeH1Size() {
+//     var chnageSize = document.getElementById("h1_size")
+//     chnageSize.style = "font-size:10vh"
+// }
+
+// function changeText() {
+//     var changeSpanText = document.getElementById("p_text")
+//     changeSpanText.innerText = "thats is Goodd"
+// }
+
+// function printUserName() {
+//     var userName = prompt("enter your name")
+//     document.body.innerHTML+=(userName)
+// }
+
+function chackTimeAndChangeColor() {
+    var myDate = new Date()
+    if (myDate>) {
+        
+    }
+    
+}
