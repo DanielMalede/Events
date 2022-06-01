6bff83e906eb28fa1b499fdfba820f414d0953ce
DanielMalede <danielmalede@gmail.com>
2022-06-01 19:21:05 +0300

    .

--- main.js
+++ main.js
@@ -23,13 +23,54 @@
 
 // function printUserName() {
 //     var userName = prompt("enter your name")
-//     document.body.innerHTML+=(userName)
+//     document.body.innerHTML+=("<input type="+userName"/>)
 // }
 
-function chackTimeAndChangeColor() {
-    var myDate = new Date()
-    if (myDate>) {
+// function chackTimeAndChangeColor() {
+//     var mySetTime = new Date()
+//     var newTime = mySetTime.getHours()
+//     var titleh1 = document.getElementById("H1_color")
+//     if (newTime<17) {
+//         document.body.style.backgroundColor ="yellow"    
+//         titleh1.innerText="good day"
+//     }
+//     else{
+//         document.body.style.backgroundColor ="blue"    
+//         titleh1.innerText="good evning"
+//     }
+// }
+
+// function chackTimeAndChangeColor() {
+//     var userAge = +prompt("enter your age")
+//     var h1_Text = document.getElementById("welcome")
+//     if (userAge>18) {
+//         h1_Text.innerText = "Welacome"
+//     h1_Text.style = "color: red"
+//     }
+//     else{
+//         h1_Text.innerText = "Welacome"
+//         h1_Text.style = "color: blue"
+//     }
+// }
+// function hovercolor() {
+//     var h1_Text = document.getElementById("welcome")
+//     h1_Text.style = "color: green"
+// }
+
+function lotu() {
+    var theButton = document.createElement("button")
+    theButton.innerText = "click me"
+    document.body.append(theButton)
+    var rndNum = Math.floor(Math.random()*57) 
+    var allUserNumber = []
+    for ( i = 0; i < 5; i++) {
+        var userNumber = +prompt("enter number")
+        allUserNumber.push(userNumber) 
+    }
+    for (var i = 0; i < 5; i++) {
+        theButton.onclick=function(){ theButton.innerText+=(allUserNumber[i])}
         
     }
-    
+
 }
+lotu()
